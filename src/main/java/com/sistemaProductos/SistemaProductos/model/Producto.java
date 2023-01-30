d94de3ea75fd50794418fc851038e7440d783e7a
leonelSubelza <subelza150@gmail.com>
2023-01-30 17:28:58 -0300

    Se agrega modelo de producto y se agrega dependencia JPARepository

---
+++ src/main/java/com/sistemaProductos/SistemaProductos/model/Producto.java
@@ -0,0 +1,60 @@
+package com.sistemaProductos.SistemaProductos.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table( name = "producto")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "nombre", nullable = false, length = 150)
+	private String nombre;
+	
+	@Column(name = "descripcion", length = 150)
+	private String descripcion;
+	
+	@Column(name = "precio", nullable = false, length = 150)
+	private String precio;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+}
