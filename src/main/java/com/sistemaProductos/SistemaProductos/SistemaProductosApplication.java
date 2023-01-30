fc06fa7fe866e07b95e4b427bb4900e4096e982e
leonelSubelza <subelza150@gmail.com>
2023-01-30 17:01:01 -0300

    Se agrega proyecto base

---
+++ src/main/java/com/sistemaProductos/SistemaProductos/SistemaProductosApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaProductos.SistemaProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaProductos/SistemaProductos/SistemaProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaProductos.SistemaProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
