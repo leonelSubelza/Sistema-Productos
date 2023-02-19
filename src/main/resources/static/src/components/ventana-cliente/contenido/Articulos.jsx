8f0a9c5db1c6f7e9d143ee9bc1dc6a583ab11e21
GiovanniSia <76817363+GiovanniSia@users.noreply.github.com>
2023-02-18 23:30:29 -0300

    Agrega categoria para las tarjetas en la ventana cliente

--- src/main/resources/static/src/components/ventana-cliente/contenido/Articulos.jsx
+++ src/main/resources/static/src/components/ventana-cliente/contenido/Articulos.jsx
@@ -1,31 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import Articulo from './Articulo';
-import { cargarObjetos } from "../../../service/GestionProductos";
 import Paginacion from './Paginacion';
+import { funcionesContext } from '../../../context/FuncionesTablaContext';
+import { useContext } from 'react';
+
 
 function Articulos() {
-  const [productos, setproductos] = useState([]);
-  const totalProductos = productos.length;
-
+  const { actualizarTablaGenerica, borrarProductoGenerico } =
+    useContext(funcionesContext);
+  const [productos, setProductos] = useState([]);
+  const [tiposProductos, setTiposProductos] = useState([]);
+  const [listaProductos] = useState([]);
+  const [totalProductos, settotalProductos] = useState(listaProductos.length);
   const [productosPorPagina] = useState(8);
-  const [paginaActual, setpaginaActual] = useState(1);
+  const [paginaActual, setPaginaActual] = useState(1);
+  const ultimoIndex = paginaActual * productosPorPagina;
+  const primerIndex = ultimoIndex - productosPorPagina;
 
-  const ultimoIndex = paginaActual * productosPorPagina; // 2 * 4 = 8
-  const primerIndex = ultimoIndex - productosPorPagina; // 8 - 4 = 4
-
-  const cargarProductosLista = () => {
-    cargarObjetos("productos")
-      .then((response) => {
-        setproductos(response);
-      })
-      .catch(() => {
-        setproductos([]);
+  const cargarValores = (productosBD) => {
+    let productosPiolas = [];
+    let tiposProductos = [];
+    productosBD.forEach((tipoProd) => {
+      let tipoProductoObj = {
+        id: tipoProd.id,
+        nombre: tipoProd.nombre,
+      };
+      tipoProd.productos.forEach((prod) => {
+        prod.tipoProducto = tipoProductoObj;
+        productosPiolas.push(prod);
       });
-  }
+
+      tiposProductos.push(tipoProductoObj);
+    });
+    setTiposProductos(tiposProductos);
+    setProductos(productosPiolas);
+
+    setPaginaActual(1);
+    settotalProductos(productosPiolas.length)
+  };
+
 
   useEffect(() => {
-    cargarProductosLista();
-  }, []);
+    actualizarTablaGenerica("tiposProductos").then((res) => {
+      cargarValores(res);
+    });
+  }, [actualizarTablaGenerica]);
 
   return (
     <>
@@ -33,24 +52,17 @@ function Articulos() {
         <div className="container d-flex justify-content-center align-items-center h-100">
           <div className="row w-100">
             {
+              productos &&
               productos.map(prod => (
                 <div className="col-md-3 col-sm-6 col-xs-6 p-1" key={prod.id}>
-                  <Articulo imageSource={prod.imagen} nombreProducto={prod.nombre} nombreCategoria={prod.descripcion} precio={prod.precio} />
+                  <Articulo imageSource={prod.imagen} nombreProducto={prod.nombre} nombreCategoria={prod.tipoProducto.nombre} precio={prod.precio} />
                 </div>
               )).slice(primerIndex, ultimoIndex)
-
-
-              // tipoProducto.map(productos => (
-              //   productos.productos.map(prod => (
-              //     <div className="col-md-3 col-sm-6 col-xs-6 p-1" key={prod.id}>
-              //       <Articulo imageSource={prod.imagen} nombreProducto={prod.nombre} nombreCategoria={productos.nombre} precio={prod.precio} />
-              //     </div>
-              //   ))).slice(primerIndex, ultimoIndex))
             }
           </div>
         </div>
       </div>
-      <Paginacion productosPorPagina={productosPorPagina} paginaActual={paginaActual} setpaginaActual={setpaginaActual} totalProductos={totalProductos} />
+      <Paginacion productosPorPagina={productosPorPagina} paginaActual={paginaActual} setpaginaActual={setPaginaActual} totalProductos={totalProductos} />
     </>
   )
 }
