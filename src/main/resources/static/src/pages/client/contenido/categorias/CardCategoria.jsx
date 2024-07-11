bbd412b61a7f37456b8ac02adb5d203aab2cfcd9
leonelSubelza <subelza150@gmail.com>
2024-07-11 18:02:40 -0300

    Se agregan modificaciones para contenido.css

--- src/main/resources/static/src/pages/client/contenido/categorias/CardCategoria.jsx
+++ src/main/resources/static/src/pages/client/contenido/categorias/CardCategoria.jsx
@@ -6,6 +6,7 @@ export default function CardCategoria({handleCardSet,tipoProducto}) {
   return (
     <>
       <div className={`card-categoria-container-item`} onClick={handleClick}>
+        <div className={'card-categoria-img-container'}><img alt={'Imagen de ilustración de Categoria'} src={'https://static.vecteezy.com/system/resources/previews/000/581/914/non_2x/tshirt-icon-vector-illustration.jpg'} /></div>
         <p>{tipoProducto.nombre}</p>
       </div>
     </>
--- src/main/resources/static/src/pages/client/contenido/categorias/CategoriasContainer.jsx
+++ src/main/resources/static/src/pages/client/contenido/categorias/CategoriasContainer.jsx
@@ -8,70 +8,91 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/grid';
 import 'swiper/css/pagination';
-import { Grid, Pagination } from 'swiper/modules';
-import {useSelector} from "react-redux";
-import {useEffect, useState} from "react";
-
+import { Pagination } from 'swiper/modules';
+import { v4 as uuidv4 } from 'uuid';
 export default function CategoriasContainer({ show, handleCategoriaShow }) {
     // const { tiposProductos } = useContext(funcionesContext);
 
-    const tiposProductos = useSelector(store => store.productsType.value);
-    const [rows, setRows] = useState(1)
-
-    // const [indexCategoriaShow, setIndexCategoriaShow] = useState(0);
+    // const tiposProductos = useSelector(store => store.productsType.value);
+    const tiposProductos = [
+        {
+            "id": 1,
+            "nombre": "REMERA PARA PUTITAS BARATAS"
+        },
+        {
+            "id": 2,
+            "nombre": "REMERA"
+        },
+        {
+            "id": 3,
+            "nombre": "PANTALON"
+        },
+        {
+            "id": 4,
+            "nombre": "ZAPATILLAS"
+        },
+        {
+            "id": 5,
+            "nombre": "REMERA XD"
+        },
+        {
+            "id": 6,
+            "nombre": "REMERA WORLD"
+        },
+        {
+            "id": 7,
+            "nombre": "REMERA FOX SPORTS PLAY +"
+        },
+        {
+            "id": 8,
+            "nombre": "REMERA LIGTH"
+        }
+    ]
 
     const handleCardSet = (tipoProducto) => {
         return handleCategoriaShow(tipoProducto);
     }
 
-    useEffect(() => {
-        if(tiposProductos.length>=4){
-            setRows(2);
-        }else{
-            setRows(1);
-        }
-    }, [tiposProductos]);
-
     return (
-        <>
-            <div className={`categorias-container ${show && 'show'}`}>
-                <div className={'categorias-titulo-container'}><h1>Categorías</h1></div>
-                <div className={'card-categoria-container'}>
-                    <Swiper
-                        slidesPerView={3}
-                        grid={{
-                          rows: 1,
-                        }}
-                        spaceBetween={30}
-                        pagination={{
-                          clickable: true,
-                        }}
-                        breakpoints={{
-                            0: {
-                                slidesPerView: 1,
-                                spaceBetween: 10,
-                            },
-                            700: {
-                                slidesPerView: 3,
-                                spaceBetween: 30,
-                            },
-                        }}
-                        modules={[Grid, Pagination]}
-                        className="mySwiper"
-                    >
-                        {/*{tiposProductos && tiposProductos.length>0 &&*/}
-                        {tiposProductos.map((tipoProducto, index) => (
-                            <SwiperSlide key={index}>
-                                <CardCategoria
-                                    // show={indexCategoriaShow === tipoProducto.id}
-                                    handleCardSet={() => handleCardSet(tipoProducto)}
-                                    tipoProducto={tipoProducto}
-                                />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </div>
-            </div>
-        </>
+      <>
+          <div className={`categorias-container ${show && 'show'}`}>
+              <div className={'categorias-titulo-container'}><h1>Categorías</h1></div>
+              <div className={'card-categoria-container'}>
+                  <Swiper
+                    slidesPerView={1}
+                    spaceBetween={10}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 4,
+                            spaceBetween: 40,
+                        },
+                        1024: {
+                            slidesPerView: 5,
+                            spaceBetween: 50,
+                        },
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                  >
+                      {tiposProductos && tiposProductos.map((tipoProducto) => (
+                        <SwiperSlide key={uuidv4()}>
+                            <CardCategoria
+                              // show={indexCategoriaShow === tipoProducto.id}
+                              handleCardSet={() => handleCardSet(tipoProducto)}
+                              tipoProducto={tipoProducto}
+                            />
+                        </SwiperSlide>
+                      ))}
+                  </Swiper>
+              </div>
+          </div>
+      </>
     );
 }
