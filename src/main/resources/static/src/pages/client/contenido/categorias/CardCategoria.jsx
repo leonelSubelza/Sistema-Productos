c4b413e6f5e98fc22cf64cf6475975b792486cdf
leonelSubelza <subelza150@gmail.com>
2024-07-12 18:23:22 -0300

    Update

--- src/main/resources/static/src/pages/client/contenido/categorias/CardCategoria.jsx
+++ src/main/resources/static/src/pages/client/contenido/categorias/CardCategoria.jsx
@@ -1,4 +1,4 @@
-import {IMAGES_URL} from "../../../../service/Configuracion.js";
+import {IMAGES_URL_PRODUCT_TYPE} from "../../../../service/Configuracion.js";
 
 export default function CardCategoria({handleCardSet,tipoProducto}) {
   const handleClick = () => {
@@ -10,7 +10,7 @@ export default function CardCategoria({handleCardSet,tipoProducto}) {
       <div className={`card-categoria-container-item`} onClick={handleClick}>
         <div className={'card-categoria-img-container'}>
           {tipoProducto.imagen && tipoProducto.imagen!=='' ?
-            <img alt={'Imagen de ilustración de Categoria'} src={`${IMAGES_URL}${tipoProducto.imagen}?timestamp=${new Date().getTime()}`} />
+            <img alt={'Imagen de ilustración de Categoria'} src={`${IMAGES_URL_PRODUCT_TYPE}${tipoProducto.imagen}?timestamp=${new Date().getTime()}`} />
             :
             <img alt={'Imagen de ilustración de Categoria'} src={'https://static.vecteezy.com/system/resources/previews/000/581/914/non_2x/tshirt-icon-vector-illustration.jpg'} />
           }
