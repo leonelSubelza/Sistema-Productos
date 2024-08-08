7bddd3880384f1ff8b5889e4474dc3a7d7a22889
leonelSubelza <subelza150@gmail.com>
2024-08-08 16:46:11 -0300

    Se añaden textos por default para cuando no haya nada configurado

--- src/main/resources/static/src/pages/client/inicio/hero/Hero.jsx
+++ src/main/resources/static/src/pages/client/inicio/hero/Hero.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Hero.css";
 import {useSelector} from "react-redux";
 
-
+const DEFAULT_TITLE_MESSAGE = 'Tus productos a la vista';
+const DEFAULT_SUBTITLE_MESSAGE = ' ¡Bienvenidos! Aqui podran subir sus productos para ser mostrador al publico.'
 
 const Inicio = () => {
   const pageDetails = useSelector((store) => store.pageDetails);
-  // const [previewDetails, setPreviewDetails] = useState(pageDetails);
+  const [pageTitle, setPageTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+
+  useEffect(() => {
+    if(!pageDetails.title || pageDetails.title==='') {
+      setPageTitle(DEFAULT_TITLE_MESSAGE);
+    }else{
+      setPageTitle(pageDetails.title);
+    }
+    if(!pageDetails.descriptionTitle || pageDetails.descriptionTitle==='') {
+      setSubtitle(DEFAULT_SUBTITLE_MESSAGE);
+    }else{
+      setPageTitle(pageDetails.descriptionTitle);
+    }
+  }, []);
+
   return (
     <div className="hero-section">
       <div className="hero-text">
-        <h1>{pageDetails.title}</h1>
+        <h1>{pageTitle}</h1>
         <p>
-          {pageDetails.descriptionTitle}
+          {subtitle}
         </p>
         <a href="#producto-link">
         <button>Catálogo</button>
--- src/main/resources/static/src/pages/client/nosotros/Nosotros.jsx
+++ src/main/resources/static/src/pages/client/nosotros/Nosotros.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./nosotros.css";
 import {useNavigate} from "react-router";
 import {PublicRoutes} from "../../../router/routes.js";
 import {useSelector} from "react-redux";
 
+const DEFAULT_ABOUT_US_TEXT = 'En Tienda Humilde nos centramos en ofrecer a nuestros clientes que puedan vender sus productos de la forma más humilde posible.'
+
 const Nosotros = () => {
   const navigate = useNavigate();
   const pageDetails = useSelector((store) => store.pageDetails);
+  const [description, setDescription] = useState('');
 
   const handleUnirse = () => {
     navigate(PublicRoutes.LOGIN)
   }
 
+  useEffect(() => {
+    if(!pageDetails.pageDescription || pageDetails.pageDescription === '') {
+      setDescription(DEFAULT_ABOUT_US_TEXT);
+    }else{
+      setDescription(pageDetails.pageDescription);
+    }
+  }, []);
+
   return (
     <div id="nosotros-link" className="nosotros">
       <h1>Nosotros</h1>
       <p>
-        {pageDetails.pageDescription}
-{/*        En Tienda Humilde nos centramos en ofrecer a nuestros clientes que
-        puedan vender sus productos de la forma más humilde posible.{" "}*/}
+        {description}
       </p>
       <a>
       <button onClick={handleUnirse}>Unirse</button>
